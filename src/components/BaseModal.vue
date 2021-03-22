0fa517a3f0f6bbc8f0aca2ef1df40a9279d0976b
David Velez <dveleztx@gmail.com>
2021-03-22 00:04:51 -0500

    First commit, using animation and keyframes in CSS for basic animations.

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="backdrop" @click="$emit('close')"></div>
+  <dialog open>
+    <slot></slot>
+  </dialog>
+</template>
+
+<script scoped>
+export default {
+  emits: ['close'],
+};
+</script>
+
+<style scoped>
+.backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: 10;
+  background-color: rgba(0, 0, 0, 0.75);
+}
+
+dialog {
+  position: fixed;
+  top: 30vh;
+  width: 30rem;
+  left: calc(50% - 15rem);
+  margin: 0;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  border-radius: 12px;
+  padding: 1rem;
+  background-color: white;
+  z-index: 100;
+  border: none;
+  /* This doesn't work for some reason, ongoing vue issue: https://github.com/vuejs/vue-next/issues/3304 */
+  animation: modal 0.3s ease-out forwards;
+}
+</style>
+
+<!-- Have a second styles tag for keyframes, because of bug above, this one unscoped -->
+<style>
+@keyframes modal {                                                                                                      
+  from {
+    opacity: 0;
+    transform: translateY(-50px) scale(0.9);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0) scale(1);;
+  }
+}
+</style>
