493dfefc2194d82ba251f647c17e1ea517fd38b0
David Velez <dveleztx@gmail.com>
2021-03-27 00:13:40 -0500

    Updated transitions to use the special CSS classes to demonstate their use, and how they can use animations when a DOM is mounted and removed.

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
-    <transition>
+    <transition name="para">
       <p v-if="paraIsVisible">This is only sometimes visible...</p>
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
@@ -99,32 +99,34 @@ button:active {
   animation: slide-scale 0.3s ease-out forwards;
 }
 
-.v-enter-from {
-  opacity: 0;
-  transform: translateY(-30px);
+.para-enter-from {
+  /*opacity: 0;
+  transform: translateY(-30px);*/
 }
 
-.v-enter-active {
-  transition: all 0.3s ease-out;
+.para-enter-active {
+  /*transition: all 0.3s ease-out;*/
+  animation: slide-scale 0.3s ease-out;
 }
 
-.v-enter-to {
-  opacity: 1;
-  transform: translateY(0);
+.para-enter-to {
+  /*opacity: 1;
+  transform: translateY(0);*/
 }
 
-.v-leave-from {
-  opacity: 1;
-  transform: translateY(0);
+.para-leave-from {
+  /*opacity: 1;
+  transform: translateY(0);*/
 }
 
-.v-leave-active {
-  transition: all 0.3s ease-in;
+.para-leave-active {
+  animation: slide-scale 0.3s ease-out;
+  /*transition: all 0.3s ease-in;*/
 }
 
-.v-leave-to {
-  opacity: 0;
-  transform: translateY(30px);
+.para-leave-to {
+  /*opacity: 0;
+  transform: translateY(30px);*/
 }
 
 @keyframes slide-scale {
